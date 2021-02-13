8859abfbf68fe4bcdde4b37b11a98870ded3ed8c
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 19:07:04 -0300

    Add files via upload

---
+++ Largura e Comprimento/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Largura_e_Comprimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            Console.WriteLine();
+            Console.Write("Digite a Largura : ");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write("Digite o Comprimento : ");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write("Digite a Preço do metro quadrado : ");
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine();
+            Console.WriteLine("Area = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine("Preço = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
